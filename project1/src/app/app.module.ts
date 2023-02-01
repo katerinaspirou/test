69b961033c20d42506a406e3e0bef22eb86d9656
katerinaspirou <spirouu17@gmail.com>
2023-02-01 22:01:36 +0200

    title

---
+++ project1/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Com1Component } from './com1/com1.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Com1Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
